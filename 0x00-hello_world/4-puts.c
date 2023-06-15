09bb235fe065a7e2e69ff2481739407239f962ae
Ihuoma Favour Agbaru <agbaruihuoma@gmail.com>
2023-06-15 14:48:57 -0700

    description implemented for each codes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 
 #include <stdio.h>
 
-/* this program prints a statement */
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
