9e0b9f545e531fe7aee707d656ac2d1c93855cfa
Ihuoma Favour Agbaru <agbaruihuoma@gmail.com>
2023-06-15 14:41:08 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,6 +4,6 @@
 /* this program prints a statement */
 int main(void)
 {
-       puts ("\"Programming is like building a multilingual puzzle");	
-	return (0);       
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,6 @@
 /* prints with the print reserved word */
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
