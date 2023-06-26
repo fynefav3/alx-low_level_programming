9e334f2380296b2c0dcdbf96c67f12f10b3d076d
Ihuoma Favour Agbaru <agbaruihuoma@gmail.com>
2023-06-26 15:44:14 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/* prits every minutes of the day starting from
- * 00:00 to 23:59
- *
- *  */
+/* 
+ *  * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
+ */
  void jack_baeur(void)
 {
 	int hour, minute;
