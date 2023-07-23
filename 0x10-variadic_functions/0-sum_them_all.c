df9caf0e8ffa7784d5ae8938918f40df38109e75
Ihuoma Favour Agbaru <agbaruihuoma@gmail.com>
2023-07-23 08:21:44 -0700

    fixed error

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,18 +9,22 @@
  * Return: If n == 0 - 0.
  *         Otherwise - the sum of all parameters.
  */
+
+
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i, sum = 0;
+	unsigned int param, sum = 0;
 
+	/* initialize the argument list from the start */
 	va_start(ap, n);
 
-	for (i = 0; i < n; i++)
+	/* iterate through all parameter values*/
+	for (param = 0; param < n; param++)
+		/* get the next parameter value and add it to sum*/
 		sum += va_arg(ap, int);
-
+	/*Clean up*/
 	va_end(ap);
 
 	return (sum);
 }
-
