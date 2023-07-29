82ed8ed1cd55e0973465aa387fe32901564437e0
Ihuoma Favour Agbaru <agbaruihuoma@gmail.com>
2023-07-29 14:07:22 -0700

    corrected betty error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,25 +2,28 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
 /**
- * prints_list - This function prints a list in a new line.
- * @list_h: is a const
- * @h: is a const
+ * print_list - This function prints a list in a new line.
+ * @h: name of the list
  *
- * Return: Count.
+ * Return: number of nodes and count.
  */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
 
-size_t print_list(const list_t *h) {
-  int count = 0;
-  while (h != NULL) {
-    if (h->str == NULL) {
-      printf("[0] (nil)\n");
-    } else {
-      printf("[%lu] %s\n", h->len, h->str);
-    }
-    h = h->next;
-    count++;
-  }
-  return count;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%lu] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
