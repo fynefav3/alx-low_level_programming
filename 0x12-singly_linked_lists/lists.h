2aeac608b82f758751bd6f834af0c3a0f526f21a
Ihuoma Favour Agbaru <agbaruihuoma@gmail.com>
2023-07-29 14:14:58 -0700

    done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,19 @@
 #ifndef LIST_H
 #define LIST_H
 
-typedef struct list_t {
-  char *str;
-  size_t len;
-  struct list_t *next;
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 
